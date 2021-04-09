f81b2aac53a890f61e83dde16f00c27150decd01
Shaun Wassell <shaun@trove.ai>
2021-04-09 17:04:59 -0400

    Finished server

--- src/server.js
+++ src/server.js
@@ -1,61 +1,70 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+import path from 'path';
 
-const articlesInfo = [{
-    name: 'learn-react',
-    upvotes: 0,
-    comments: [],
-}, {
-    name: 'learn-node',
-    upvotes: 0,
-    comments: [],
-}, {
-    name: 'my-thoughts-on-resumes',
-    upvotes: 0,
-    comments: [],
-}];
+const startServer = async () => {
+    const app = express();
+    app.use(express.static(path.join(__dirname, '/build')));
+    app.use(express.json());
 
-const app = express();
+    const client = await MongoClient.connect('mongodb://localhost:27017', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = client.db('react-blog-db-apr-2021');
+    
+    // Loading information about an article
+    // GET /articles/:name --> { upvotes: 1, comments: [...] }
+    app.get('/articles/:name', async (req, res) => {
+        const { name } = req.params;
+        const articleInfo = await db.collection('articles').findOne({ name });
+    
+        if (!articleInfo) return res.status(404).json({ message: 'No article with that name found' });
+    
+        res.status(200).json(articleInfo);
+    });
+    
+    // Adding upvotes to an article
+    // PUT /articles/:name/upvotes --> { upvotes: 2, comments: [...] }
+    app.put('/articles/:name/upvotes', async (req, res) => {
+        const { name } = req.params;
 
-app.use(express.json());
+        await db.collection('articles').updateOne(
+            { name },
+            { $inc: { upvotes: 1 } }
+        );
 
-// Loading information about an article
-// GET /articles/:name --> { upvotes: 1, comments: [...] }
-app.get('/articles/:name', (req, res) => {
-    const { name } = req.params;
-    const articleInfo = articlesInfo.find(article => article.name === name);
+        const articleInfo = await db.collection('articles').findOne({ name });
+    
+        if (!articleInfo) return res.status(404).json({ message: 'No article with that name found' });
+    
+        res.status(200).json(articleInfo);
+    });
+    
+    // Adding comments to an article
+    // POST /articles/:name/comments { author: 'Shaun', text: 'Great!' }
+    // -----> { upvotes: 1, comments: [{ author: ... }] }
+    app.post('/articles/:name/comments', async (req, res) => {
+        const { name } = req.params;
+        const { author, text } = req.body;
 
-    if (!articleInfo) return res.status(404).json({ message: 'No article with that name found' });
+        await db.collection('articles').updateOne(
+            { name },
+            { $push: { comments: { author, text } } },
+        );
 
-    res.status(200).json(articleInfo);
-});
+        const articleInfo = await db.collection('articles').findOne({ name });
+    
+        if (!articleInfo) return res.status(404).json({ message: 'No article with that name found' });
+    
+        res.status(200).json(articleInfo);
+    });
 
-// Adding upvotes to an article
-// PUT /articles/:name/upvotes --> { upvotes: 2, comments: [...] }
-app.put('/articles/:name/upvotes', (req, res) => {
-    const { name } = req.params;
-    const articleInfo = articlesInfo.find(article => article.name === name);
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/build/index.html'));
+    });
+    
+    app.listen(8080, () => console.log('Server is listening on port 8080'));
+}
 
-    if (!articleInfo) return res.status(404).json({ message: 'No article with that name found' });
-
-    articleInfo.upvotes += 1;
-
-    res.status(200).json(articleInfo);
-});
-
-// Adding comments to an article
-// POST /articles/:name/comments { author: 'Shaun', text: 'Great!' }
-// -----> { upvotes: 1, comments: [{ author: ... }] }
-app.post('/articles/:name/comments', (req, res) => {
-    const { name } = req.params;
-    const { author, text } = req.body;
-
-    const articleInfo = articlesInfo.find(article => article.name === name);
-
-    if (!articleInfo) return res.status(404).json({ message: 'No article with that name found' });
-
-    articleInfo.comments.push({ author, text });
-
-    res.status(200).json(articleInfo);
-});
-
-app.listen(8080, () => console.log('Server is listening on port 8080'));
+startServer();
